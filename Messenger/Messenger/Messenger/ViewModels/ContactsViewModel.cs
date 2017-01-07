edd399024401d97c779b8da20e09d42b128ab518
KhrenovMaxim <khrenoffmaxim@yandex.ru>
2017-01-07 23:40:18 +0400

    first commit

---
+++ Messenger/Messenger/Messenger/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Messenger.Models;
+using Messenger.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace Messenger.ViewModels
+{
+    public class ContactsViewModel
+    {
+       
+           public ObservableCollection<ContactModel> Contacts { get; set; } = new ObservableCollection<ContactModel>
+           {
+               new ContactModel
+               {
+                   FullName = "Максим Хренов",
+                   LastMessage = "Приветос",
+                   Photo = "https://vk.com/hrenovmax?z=photo182832753_367676578%2Falbum182832753_0%2Frev",
+               },
+
+           };
+
+      
+
+        public ICommand ProfileCommand { get; set; }
+
+        public ICommand ExitCommand { get; set; }
+
+
+        
+
+        private Page _page;
+
+        public ContactsViewModel(Page page)
+        {
+            _page = page;
+
+            ExitCommand = new Command(OpenExit);
+            ProfileCommand = new Command(OpenProfile);
+        }
+
+
+        private async void OpenProfile()
+        {
+            await _page.Navigation.PushAsync(new ProfilePage());
+        }
+
+        public async Task OpenChat()
+        {
+            await _page.Navigation.PushAsync(new ChatPage());
+        }
+
+        private async void OpenExit()
+        {
+            await _page.Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
---
+++ Messenger/Messenger/Messenger/ViewModels/LoginViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Messenger.Views;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Messenger.ViewModels
+{
+    public class LoginViewModel 
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public ICommand LoginCommand { get; set; }
+
+        public ICommand RegisterCommand { get; set; }
+
+        private Page _page;
+
+        public LoginViewModel(Page page)
+        {
+            _page = page;
+            LoginCommand = new Command(OpenContacts);
+            RegisterCommand = new Command(OpenRegister);
+        }
+
+        private async void OpenContacts()
+        {
+            Debug.WriteLine($"UserName:{UserName}, Password:{Password}");
+            await _page.Navigation.PushAsync(new ContactsPage());
+        }
+        private async void OpenRegister()
+        {
+            await _page.Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
